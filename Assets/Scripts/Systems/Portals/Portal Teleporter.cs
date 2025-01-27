4d567acdf5e56e4f62c33e114c274c8e75bee6a7
Drioh <darioromano212@gmail.com>
2025-01-27 23:49:25 +0100

    Aggiustati i portali

--- Assets/Scripts/Systems/Portals/Portal Teleporter.cs
+++ Assets/Scripts/Systems/Portals/Portal Teleporter.cs
@@ -7,23 +7,34 @@ public class PortalTeleporter : MonoBehaviour
 {
     public Transform player;
     public Transform receiver;
+    [SerializeField]private bool arrival;
+    private bool active = true;
 
     private bool playerIsOverlapping = false;
 
+    void Start(){
+        if (arrival){
+            active = !active;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (playerIsOverlapping)
         {
             Vector3 portalToPlayer = player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-            if (dotProduct > 0f)
+            float dotProduct = Vector3.Dot(transform.forward, portalToPlayer);
+            print("sono "+this+" "+receiver.rotation);
+            if (dotProduct > 0f && active)
             {
-                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                float rotationDiff = Quaternion.Angle(transform.rotation, receiver.rotation);
+                if(arrival){
+                    rotationDiff *=-1f;
+                }
                 rotationDiff += 180;
                 player.Rotate(Vector3.up, rotationDiff);
                 Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f)*portalToPlayer;
-                player.position = receiver.position + positionOffset + new Vector3(0.46f,0,0);
+                player.position = receiver.position + positionOffset;
 
                 playerIsOverlapping = false;
             }
@@ -43,6 +54,7 @@ public class PortalTeleporter : MonoBehaviour
         if (other.tag == "Player")
         {
             playerIsOverlapping = false;
+            active = !active;
         }
     }
 }
--- Assets/Scripts/Systems/Portals/PortalCamera.cs
+++ Assets/Scripts/Systems/Portals/PortalCamera.cs
@@ -7,15 +7,21 @@ public class PortalCamera : MonoBehaviour
     public Transform playerCamera;
     public Transform portal;
     public Transform otherPortal;
+    [SerializeField] private bool arrival;
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 playerOffsetFromPortal = playerCamera.position- otherPortal.position;
-        transform.position = portal.position + playerOffsetFromPortal;
+        //Vector3 playerOffsetFromPortal = playerCamera.position- otherPortal.position;
+        Vector3 relativePosition = otherPortal.InverseTransformPoint(playerCamera.position);
+        Vector3 pos = portal.TransformPoint(relativePosition);
+        transform.position = pos;
+        //transform.position = portal.position + playerOffsetFromPortal;
 
         float angularDifferenceBetweenPortalRotation = Quaternion.Angle(portal.rotation, otherPortal.rotation);
-
+        if (arrival){
+            angularDifferenceBetweenPortalRotation*=-1;
+        }
         Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotation, Vector3.up);
         Vector3 newCameraDirection = portalRotationalDifference*playerCamera.forward;
         transform.rotation = Quaternion.LookRotation(newCameraDirection,Vector3.up);
