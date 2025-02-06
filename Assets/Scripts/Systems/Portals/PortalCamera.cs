a925e6f6d13f4de11e5beb5d55ad6ed9f7ad85d3
Simone Aronica <public.simonearonica@protonmail.ch>
2025-02-06 16:05:56 +0100

    Fixed portal misalignment when changing camera FOV

--- Assets/Scripts/Systems/Portals/PortalCamera.cs
+++ Assets/Scripts/Systems/Portals/PortalCamera.cs
@@ -13,25 +13,36 @@ public class PortalCamera : MonoBehaviour
 
     [SerializeField] private bool arrival;
 
+    private Camera playerCam;
+    private Camera portalCam;
+
+    void Start()
+    {
+        playerCam = playerCamera.GetComponent<Camera>();
+        portalCam = GetComponent<Camera>();
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {
-        //Vector3 playerOffsetFromPortal = playerCamera.position- otherPortal.position;
+        // Sincronizza il FOV della camera del portale con quello della camera del giocatore
+        portalCam.fieldOfView = playerCam.fieldOfView;
+
         Vector3 relativePosition = otherPortal.InverseTransformPoint(playerCamera.position);
         Vector3 pos = portal.TransformPoint(relativePosition);
         transform.position = pos;
-        //transform.position = portal.position + playerOffsetFromPortal;
 
         float angularDifferenceBetweenPortalRotation = Vector3.Angle(fence.forward, otherFence.forward);
-        if (arrival){
-            print("rotazione di "+fence+"è "+fence.rotation);
-            print("rotazione di "+otherFence+"è "+otherFence.rotation);
-            print("rotazione di "+otherPortal+"è "+otherPortal.rotation);
+        if (arrival)
+        {
+            print("rotazione di " + fence + " è " + fence.rotation);
+            print("rotazione di " + otherFence + " è " + otherFence.rotation);
+            print("rotazione di " + otherPortal + " è " + otherPortal.rotation);
 
-            angularDifferenceBetweenPortalRotation*=-1;
+            angularDifferenceBetweenPortalRotation *= -1;
         }
         Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotation, Vector3.up);
-        Vector3 newCameraDirection = portalRotationalDifference*playerCamera.forward;
-        transform.rotation = Quaternion.LookRotation(newCameraDirection,Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
     }
 }
